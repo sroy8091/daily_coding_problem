72ef4f1c01cd4dd7061fdadd0f1104ebc8c4d67b
Sumit Roy <sroy8091@gmail.com>
2019-09-05 06:08:48 +0530

    day 1

---
+++ pair_sum_equal_to_k.py
@@ -0,0 +1,25 @@
+"""
+This problem was recently asked by Google.
+
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+"""
+
+
+def pair_sum(arr, k):
+    hash = {}
+    for i in arr:
+        if hash.get(i):
+            return True
+        else:
+            hash[abs(i-k)] = i
+    return False
+
+def main():
+    arr = [10, 15, 3, 7]
+    k = 17
+    print(pair_sum(arr, k))
+
+if __name__=="__main__":
+    main()
