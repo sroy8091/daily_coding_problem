ea95469968ca5fd9c72b1f5348912a552e0adb48
Sumit Roy <sroy8091@gmail.com>
2019-09-11 16:41:00 +0530

    day 2

---
+++ all_products_except_i.py
@@ -0,0 +1,42 @@
+"""
+This problem was asked by Uber.
+
+Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+Follow-up: what if you can't use division?
+"""
+
+def product(arr):
+    """
+    The idea is to create two array with products till 
+    """
+    left = [1]
+    right = [1] * len(arr)
+    res = []
+    p = 1
+    for i in range(1, len(arr)):
+        p = p*arr[i-1]
+        left.append(p)
+    
+    p = 1
+    for i in range(len(arr)-2, -1, -1):
+        p = p*arr[i+1]
+        right[i] = p
+    
+    for i, j in zip(left, right):
+        res.append(i*j)
+    
+    return res
+
+
+
+
+def main():
+    arr = [10, 15, 3, 7]
+    k = 17
+    print(product(arr))
+
+if __name__=="__main__":
+    main()
--- pair_sum_equal_to_k.py
+++ pair_sum_equal_to_k.py
@@ -8,6 +8,10 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
 
 def pair_sum(arr, k):
+    """
+    The idea is to create a hash and then check whether the current number is already present 
+    if not then current-k will be added
+    """
     hash = {}
     for i in arr:
         if hash.get(i):
