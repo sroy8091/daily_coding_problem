8337e44062646cf98d6806e625f84f7035616d12
Sumit Roy <sroy8091@gmail.com>
2019-09-20 20:25:33 +0530

    brevity update

--- unival_tree.py
+++ unival_tree.py
@@ -1,7 +1,8 @@
 """
 This problem was asked by Google.
 
-A unival tree (which stands for "universal value") is a tree where all nodes under it have the same value.
+A unival tree (which stands for "universal value") is a tree where all nodes under 
+it have the same value.
 
 Given the root to a binary tree, count the number of unival subtrees.
 
